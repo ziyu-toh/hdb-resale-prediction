af0ba1578c850a10534e9c157f231a64547c80ea
Zi Yu <59678777+hello-bob@users.noreply.github.com>
2025-08-25 21:28:43 +0800

    drafted code for model inference + streamlit app

---
+++ src/inference.py
@@ -0,0 +1,38 @@
+import pickle
+import polars as pl
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Load champion model
+with open('models/champion_model.pkl', 'rb') as f:
+    loaded_model = pickle.load(f)
+
+# Define input data model
+class InputData(BaseModel):
+    flat_age_years: int
+    floor_area_sqm: float
+    days_from_earliest_data: int
+    flat_type: str
+    flat_model_revised: str
+    town: str
+    
+# Initialize FastAPI app
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"hello": "world"} 
+
+@app.post("/predict")
+async def predict(input_data: InputData):
+    input_df = pl.DataFrame([input_data])
+    
+    # Prediction
+    try:
+        prediction = loaded_model.predict(input_df)
+        print("Prediction:", prediction)
+        
+    except Exception as e:
+        print("Error during prediction:", e)
+        
+    return {"Prediction": prediction[0]}
