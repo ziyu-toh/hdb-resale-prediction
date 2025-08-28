08d896929f312d457ad698645debd61d57766583
Zi Yu <59678777+hello-bob@users.noreply.github.com>
2025-08-28 09:33:29 +0800

    change in IP address for streamlit

--- app/streamlit/main.py
+++ app/streamlit/main.py
@@ -12,7 +12,7 @@ flat_model_revised = st.text_input("Flat Model (Revised)")
 town = st.text_input("Town")
 
 if st.button("Submit"):
-    fastapi_url = "http://52.74.110.47/predict" # the public url for the AWS ECS holding the FastAPI Docker container
+    fastapi_url = "http://54.169.110.104:80/predict" # the public url for the AWS ECS holding the FastAPI Docker container
     payload = {
         "flat_age_years": flat_age_years,
         "floor_area_sqm": floor_area_sqm,
