bb022ff281e1037a1dca74c8da5ed0eb58e43427
Zi Yu <59678777+hello-bob@users.noreply.github.com>
2025-09-03 11:34:44 +0800

    added unit test for fastapi POSTprediction

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,25 +3,37 @@ from fastapi.testclient import TestClient
 import numpy as np
 import polars as pl
 
+sample_json = {"town":"ANG MO KIO",
+                                "flat_type":"2 ROOM",
+                                "flat_model_revised":"Improved",
+                                "flat_age_years":46,
+                                "floor_area_sqm":44.0,
+                                "days_from_earliest_data":4323,
+                                "storey_range_grouped":"1-15"}
+
 def test_prediction():
     # Load data
-    sample_data = pl.DataFrame({"town":["ANG MO KIO"],
-                                "flat_type":["2 ROOM"],
-                                "flat_model_revised":["Improved"],
-                                "flat_age_years":[46],
-                                "floor_area_sqm":[44.0],
-                                "days_from_earliest_data":[4323],
-                                "storey_range_grouped":["1-15"]})
+    sample_data = pl.DataFrame([sample_json])
     
     # Prediction
     pred = loaded_model.predict(sample_data)
 
-    assert loaded_model is not None, "No model found"
-    assert pred is not None, "No predictions made"
-    assert isinstance(pred[0], np.float64), "Prediction is not a float"
+    assert loaded_model is not None
+    assert pred is not None
+    assert isinstance(pred[0], np.float64)
 
 def test_read_main():
     client = TestClient(app)
     response = client.get("/")
+    
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+    
+def test_app_prediction():
+    client = TestClient(app)
+    response = client.post(
+        "/predict",
+        json=sample_json,
+    )
+    assert response.status_code == 200
+    assert response.json() == {"Prediction": response.json()["Prediction"]}
