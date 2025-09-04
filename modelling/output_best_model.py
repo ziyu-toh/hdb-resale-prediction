69e8c62bffb9a9402f426f547d192f0cd3c92cfc
Zi Yu <59678777+hello-bob@users.noreply.github.com>
2025-09-04 09:10:06 +0800

    removed predictions on test set, not needed anymore with unit testing

--- modelling/output_best_model.py
+++ modelling/output_best_model.py
@@ -5,9 +5,6 @@ import pandas as pd
 # Specify best ID: To check on MLFlow
 BEST_RUN_ID = "runs:/d796c6f22a124971aa3ebce48b7916b8/model"
 
-# Load data
-test_df = pd.read_parquet('data/processed/test.parquet')
-
 # Registering best model to MLflow Model Registry
 mlflow.register_model(
     model_uri=BEST_RUN_ID, # run_id is from the experiment, not the model
@@ -16,7 +13,6 @@ mlflow.register_model(
 
 # Reloading best model, saving to models directory for deployment
 loaded_model = mlflow.sklearn.load_model(BEST_RUN_ID)
-print("Predictions: ", loaded_model.predict(test_df.drop(columns=['resale_price']).iloc[:5]))
 
 with open('models/champion_model.pkl', 'wb') as f:
     pickle.dump(loaded_model, f)
