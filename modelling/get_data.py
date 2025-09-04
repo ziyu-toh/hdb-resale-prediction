3fbb639a2e147c6a7ecccd0053b17701bd6ac818
Zi Yu <59678777+hello-bob@users.noreply.github.com>
2025-09-04 08:45:50 +0800

    converted get_data script to AWS version

--- modelling/get_data.py
+++ modelling/get_data.py
@@ -1,22 +1,29 @@
-# Test for AWS
-import urllib3
 import json
+import boto3
 import pandas as pd
+import urllib3
+from io import StringIO
 
-dataset_id = "d_8b84c4ee58e3cfc0ece0d773c8ca6abc"
-url = "https://data.gov.sg/api/action/datastore_search"
+s3 = boto3.client("s3")
+def lambda_handler(event, context):
+    bucket_name = "hdb-resale-pred-raw"
 
-# create a PoolManager for making requests
-http = urllib3.PoolManager()
+    dataset_id = "d_8b84c4ee58e3cfc0ece0d773c8ca6abc"
+    url = "https://data.gov.sg/api/action/datastore_search"
 
-# send GET request with query parameters
-response = http.request("GET", url, fields={"resource_id": dataset_id, "limit": 10})
+    # create a PoolManager for making requests
+    http = urllib3.PoolManager()
 
-# decode bytes to string and parse JSON
-data = json.loads(response.data.decode("utf-8"))
+    # send GET request with query parameters
+    response = http.request("GET", url, fields={"resource_id": dataset_id, "limit": 10})
 
-print(data["result"]["records"])
+    # decode bytes to string and parse JSON
+    data = json.loads(response.data.decode("utf-8"))
 
-# convert to dataframe
-df = pd.json_normalize(data["result"]["records"])
-print(df.shape)
+    # convert to dataframe
+    df = pd.json_normalize(data["result"]["records"])
+
+    # Save to bucket as csv
+    s3.put_object(Bucket=bucket_name, 
+                  Key="FromJan2017onwards.csv",
+                  Body=df.to_csv(index=False))
