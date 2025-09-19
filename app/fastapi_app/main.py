424077c517bd11f9459ea275b7969e7939137a36
Zi Yu <59678777+hello-bob@users.noreply.github.com>
2025-09-19 16:39:38 +0800

    champion model to downloaded from s3  to file inside fastapi_app, using github actions

--- app/fastapi_app/main.py
+++ app/fastapi_app/main.py
@@ -2,14 +2,10 @@ import joblib
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
-import boto3
 
 # Load model from s3 by downloading to tmp folder first. 
 # #Once dockerised, it has to be connected to the file path on docker
-s3_input = boto3.resource('s3', region_name='ap-southeast-1')
-input_bucket = s3_input.Bucket('hdb-resale-best-model')
-input_bucket.download_file('champion_model.joblib', '/tmp/champion_model.joblib')
-loaded_model = joblib.load('/tmp/champion_model.joblib')
+loaded_model = joblib.load('./app/fastapi_app/champion_model/champion_model.joblib')
 
 # Define input data model
 class InputData(BaseModel):
