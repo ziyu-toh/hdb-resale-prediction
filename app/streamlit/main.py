4672abcf338481c93b1ab1fe692af5a5166191fc
Zi Yu <59678777+hello-bob@users.noreply.github.com>
2025-10-01 09:03:36 +0800

    streamlit app now calculates the days_from_earliest_data variable based on the date that the app is running

--- app/streamlit/main.py
+++ app/streamlit/main.py
@@ -2,18 +2,20 @@
 import streamlit as st
 import requests
 import yaml
+from datetime import datetime, date
 
 st.title("HDB Resale Price Predictor")
 
 # Load preset data values
 with open("./app/config.yaml", "r") as file:
     config = yaml.safe_load(file)
-    print(config)
+    
+# Define days from earliest data point (1st March 2012) at the point of running the app
+days_from_earliest_data = (datetime.now().date() - date(2012, 3, 1)).days
 
 # Specify buttons in streamlit
 flat_age_years = st.number_input("Flat Age (Years)", value=0)
 floor_area_sqm = st.number_input("Floor Area (sqm)", value=0.0)
-days_from_earliest_data = st.number_input("Days from Earliest Data", value=0)
 flat_type = st.selectbox("Flat Type", config["data_flat_types"])
 storey_range_grouped = st.selectbox("Storey Range", config["data_storey_range"])
 flat_model_revised = st.selectbox("Flat Model (Revised)", config["data_flat_models"])
@@ -33,6 +35,7 @@ if st.button("Submit"):
 
     try:
         response = requests.post(fastapi_url, json=payload)
+        
         if response.status_code == 200:
             st.error(f"FastAPI response: {response.json()}")
        
