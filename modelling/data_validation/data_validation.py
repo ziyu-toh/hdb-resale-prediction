58e947be27190d6ca157a3eaf8ea8c26065b0d0c
Zi Yu <59678777+hello-bob@users.noreply.github.com>
2025-10-10 15:50:16 +0800

    added checking difference in numerical distributino for data validation

--- modelling/data_validation/data_validation.py
+++ modelling/data_validation/data_validation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import boto3
+import numpy as np
 
 def connect_to_s3():
     # Setting up AWS connection
@@ -16,11 +17,38 @@ def load_data_from_s3(input_bucket):
     
     return train_df, test_df
 
-# Check if distribution in numerical values are not too different
+# Check if distribution in numerical values are not too different using PSI
+def calculate_psi(expected_array, actual_array, buckets=10):
+    breakpoints = np.arange(0, buckets + 1) / (buckets) * 100
+    breakpoints = np.percentile(expected_array, breakpoints)
+
+    expected_percents = np.histogram(expected_array, breakpoints)[0] / len(expected_array)
+    actual_percents = np.histogram(actual_array, breakpoints)[0] / len(actual_array)
+    print(actual_percents)
+
+    psi_value = np.sum((expected_percents - actual_percents) * np.log(expected_percents / actual_percents))
+
+    return psi_value
+
+def check_numerical_distribution(train_df, test_df):
+    print("Checking numerical distribution differences using PSI...")
+    numerical_cols = ["flat_age_years", "floor_area_sqm"]
+    trigger_training = 0
+    for col in numerical_cols:
+        psi_value = calculate_psi(train_df[col], test_df[col])
+        if psi_value > 0.2:
+            print(f"Warning: Significant distribution difference in column '{col}' with PSI value: {psi_value}")
+            trigger_training += 1
+        else:
+            print(f"Column '{col}' has acceptable distribution difference with PSI value: {psi_value}")
+    
+    return trigger_training
 
 # Check if categorical values in test set are present in train set
 def check_categorical_values(train_df, test_df):
+    print("Checking categorical values in test set against training set...")
     categorical_cols = train_df.select_dtypes(include=['object', 'category']).columns.tolist()
+    trigger_training = 0
     for col in categorical_cols:
         train_unique = set(train_df[col].unique())
         test_unique = set(test_df[col].unique())
@@ -28,13 +56,23 @@ def check_categorical_values(train_df, test_df):
             print(f"Warning: Test set contains unseen categories in column '{col}'")
             unseen_categories = test_unique - train_unique
             print(f"Unseen categories: {unseen_categories}")
+            trigger_training += 1
         else:
             print(f"All categories in column '{col}' are present in the training set.")
+    
+    return trigger_training
 
-# Check if distribution in categorical values are not too different
-
-if __name__ == "__main__":
+def lambda_handler(event, context):
+    print("Starting data validation process...")
     input_bucket = connect_to_s3()
     train_df, test_df = load_data_from_s3(input_bucket)
-    check_categorical_values(train_df, test_df)
+    trigger_training_cat = check_categorical_values(train_df, test_df)
+    trigger_training_num = check_numerical_distribution(train_df, test_df)
+    if trigger_training_cat + trigger_training_num > 0:
+        print("At least one data validation check failed. Triggering model retraining.")
+        # lambda_client = boto3.client('lambda', region_name='ap-southeast-1')
+        # response = lambda_client.invoke(
+        #     FunctionName='hdb-resale-retraining',
+        #     InvocationType='RequestResponse'  # Synchronous invocation
+        # )
 
